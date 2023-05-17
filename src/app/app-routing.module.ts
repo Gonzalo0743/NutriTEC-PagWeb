0488d1189e2def2ee9e9bbd04eab110d6a890f7f
Gonzalo0743 <gonzaloa0743@gmail.com>
2023-05-16 22:30:35 -0600

    Adding node modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,39 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { MainPageComponent } from './Pages/MainPage/main-page/main-page.component';
+import { LoginAdminComponent } from './Pages/Logins/login-admin/login-admin.component';
+import { LoginClientComponent } from './Pages/Logins/login-client/login-client.component';
+import { LoginNutriComponent } from './Pages/Logins/login-nutri/login-nutri.component';
+import { RegisterClientComponent } from './Pages/Registers/register-client/register-client.component';
+import { RegisterNutriComponent } from './Pages/Registers/register-nutri/register-nutri.component';
+
+const routes: Routes = [
+  {
+    path:'',
+    component: MainPageComponent
+  },
+  {
+    path:'AdminLogin',
+    component: LoginAdminComponent
+  },
+  {
+    path:'ClientLogin',
+    component: LoginClientComponent
+  },
+  {
+    path:'NutriLogin',
+    component: LoginNutriComponent
+  },
+  {
+    path:'ClientRegister',
+    component: RegisterClientComponent
+  },
+  {
+    path:'NutriRegister',
+    component: RegisterNutriComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
